b1be4db397603e70128afa2c0217b5904f4fd478
GreedNeSS <ruslan199126@gmail.com>
2022-07-21 20:29:22 +0300

    Added new example.

---
+++ 4-TagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using _4_TagHelpers.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _4_TagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            if (person.Name == person.Email)
+            {
+                ModelState.AddModelError("", "Имя и Email не должны совпадать");
+            }
+
+            if (string.Equals(person.Name, "admin", StringComparison.OrdinalIgnoreCase))
+            {
+                ModelState.AddModelError("", "Это имя является забронированным");
+            }
+            Console.WriteLine("1");
+
+            if (ModelState.IsValid)
+            {
+                return View("Index", person);
+            }
+            return View(person);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 4-TagHelpers/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _4_TagHelpers.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Длинна имени должна быть от 2 до 30 символов")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [EmailAddress(ErrorMessage = "Некорректный адрес")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [NotMapped]
+        public string? PasswordConfirm { get; set; }
+
+        [Range(0, 110, ErrorMessage = "Возраст должен находить в диапозоне от 0 до 110")]
+        public int Age { get; set; }
+    }
+}
