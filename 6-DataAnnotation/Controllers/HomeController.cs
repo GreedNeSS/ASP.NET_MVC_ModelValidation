6ff927f6407940c8adcfce717880aeea836e6b55
GreedNeSS <ruslan199126@gmail.com>
2022-07-23 15:17:35 +0300

    Added DataAnnotation example.

---
+++ 6-DataAnnotation/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using _6_DataAnnotation.Models;
+using System;
+
+namespace _6_DataAnnotation.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index(Person? person)
+        {
+            if (person == null)
+            {
+                return View();
+            }
+
+            return View(person);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            return RedirectToAction("Index", person);
+        }
+    }
+}
---
+++ 6-DataAnnotation/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _6_DataAnnotation.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Поле обязательно к заполнению!")]
+        [Display(Name = "Имя и фамилия")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению!")]
+        [Display(Name = "Электронная почта")]
+        [UIHint("EmailAddress")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению!")]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению!")]
+        [Display(Name = "Дата рождения")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
